97acb2ec099437033ac4baab3ea1cabc9bb8d535
Clart <aclaudia.santosd@gmail.com>
2026-06-04 16:01:26 +0200

    Refactor loadTranslations function
    Removed unused else block in loadTranslations function.

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ async function loadTranslations() {
   const res = await fetch('./translations.json');
   translations = await res.json();
   applyTranslation('es');
-} else {
-  el.textContent = t[key]; // sobreescribe el texto del HTML con el del JSON
 }
 
 function applyTranslation(lang) {
